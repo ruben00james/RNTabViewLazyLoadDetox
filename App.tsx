3589e2eb427aff81de415dfa153969c8909e1656
Ruben Vlassak <ruben.vlassak@isaac.nl>
2021-12-21 12:32:51 +0100

    Tryout

--- App.tsx
+++ App.tsx
@@ -9,107 +9,108 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Dimensions, FlatList, StyleSheet, Text, View} from 'react-native';
+import {TabView, SceneMap} from 'react-native-tab-view';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// This is our placeholder component for the tabs
+// This will be rendered when a tab isn't loaded yet
+// You could also customize it to render different content depending on the route
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const UserItem = ({fullname, email}) => (
+  <>
+    <Text>{fullname}</Text>
+    <Text>{email}</Text>
+  </>
+);
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const LazyPlaceholder = ({route}) => (
+  <View style={styles.scene}>
+    <Text>Loading {route.title}…</Text>
+  </View>
+);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+export default class TabViewExample extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      {key: 'first', title: 'First'},
+      {key: 'second', title: 'Second'},
+    ],
+    dataSource: [
+      {key: 1, name: 'const abc item'},
+      {key: 2, name: 'const def item'},
+    ],
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+  componentDidMount() {
+    this.getRemoteUsers();
+  }
+
+  getRemoteUsers = () => {
+    const url = 'https://randomuser.me/api/?results=100';
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({data: res.results});
+      })
+      .catch(error => {
+        console.log('get data error from: ' + url + ' error:' + error);
+      });
+  };
+
+  renderNativeItem = item => {
+    const name = item.name.first + ' ' + item.name.last;
+    return <UserItem fullname={name} email={item.email} />;
+  };
+
+  FirstRoute = () => (
+    <View style={[styles.scene, {backgroundColor: '#ff4081'}]}>
+      <Text>Item 1</Text>
+    </View>
   );
-};
+
+  SecondRoute = () => (
+    <View style={[styles.scene, {backgroundColor: '#673ab7'}]}>
+      <FlatList
+        data={this.state.dataSource}
+        renderItem={({item}) => this.renderNativeItem(item)}
+      />
+    </View>
+  );
+
+  _handleIndexChange = (index: any) => this.setState({index});
+
+  _renderLazyPlaceholder = ({route}) => <LazyPlaceholder route={route} />;
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={SceneMap({
+          first: this.FirstRoute,
+          second: this.SecondRoute,
+        })}
+        renderLazyPlaceholder={this._renderLazyPlaceholder}
+        onIndexChange={this._handleIndexChange}
+        initialLayout={{width: Dimensions.get('window').width}}
+        style={styles.container}
+      />
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    marginTop: 50,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  scene: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
-
-export default App;
--- e2e/ClickSecondItemTest.e2e.ts
+++ e2e/ClickSecondItemTest.e2e.ts
@@ -2,8 +2,7 @@ import {element, by, expect} from 'detox';
 
 describe('Example', () => {
   it('should click on second tab and expect item 2', async () => {
-    // await element(by.text('Second')).tap();
-    // await expect(element(by.text('Item 2'))).toBeVisible();
-    await expect(element(by.text('Step One'))).toBeVisible();
+    await element(by.label('Second')).tap();
+    await expect(element(by.text('Item 2'))).toBeVisible();
   });
 });
