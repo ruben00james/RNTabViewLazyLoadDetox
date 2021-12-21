78a4d835d5fe5798e0cea72a34b8153b13cef7bd
Ruben Vlassak <ruben.vlassak@isaac.nl>
2021-12-21 17:55:32 +0100

    Converted class component to functional component

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Dimensions, FlatList, StyleSheet, Text, View} from 'react-native';
 import {TabView, SceneMap} from 'react-native-tab-view';
 
@@ -16,89 +16,82 @@ import {TabView, SceneMap} from 'react-native-tab-view';
 // This will be rendered when a tab isn't loaded yet
 // You could also customize it to render different content depending on the route
 
-const UserItem = ({fullname, email}) => (
-  <>
-    <Text>{fullname}</Text>
-    <Text>{email}</Text>
-  </>
-);
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {key: 'first', title: 'First'},
+    {key: 'second', title: 'Second'},
+  ]);
+  const [data, setData] = useState([
+    {key: 1, name: 'const abc item'},
+    {key: 2, name: 'const def item'},
+  ]);
 
-const LazyPlaceholder = ({route}) => (
-  <View style={styles.scene}>
-    <Text>Loading {route.title}…</Text>
-  </View>
-);
+  useEffect(() => {
+    getRemoteUsers();
+  }, []);
 
-export default class TabViewExample extends React.Component {
-  state = {
-    index: 0,
-    routes: [
-      {key: 'first', title: 'First'},
-      {key: 'second', title: 'Second'},
-    ],
-    dataSource: [
-      {key: 1, name: 'const abc item'},
-      {key: 2, name: 'const def item'},
-    ],
-  };
-
-  componentDidMount() {
-    this.getRemoteUsers();
-  }
-
-  getRemoteUsers = () => {
+  const getRemoteUsers = () => {
     const url = 'https://randomuser.me/api/?results=100';
     fetch(url)
       .then(res => res.json())
       .then(res => {
-        this.setState({data: res.results});
+        setData(res.results);
       })
       .catch(error => {
         console.log('get data error from: ' + url + ' error:' + error);
       });
   };
 
-  renderNativeItem = item => {
-    const name = item.name.first + ' ' + item.name.last;
-    return <UserItem fullname={name} email={item.email} />;
-  };
+  const UserItem = ({fullname, email}) => (
+    <>
+      <Text>{fullname}</Text>
+      <Text>{email}</Text>
+    </>
+  );
 
-  FirstRoute = () => (
+  const FirstRoute = () => (
     <View style={[styles.scene, {backgroundColor: '#ff4081'}]}>
       <Text>Item 1</Text>
     </View>
   );
 
-  SecondRoute = () => (
+  const SecondRoute = () => (
     <View style={[styles.scene, {backgroundColor: '#673ab7'}]}>
-      <FlatList
-        data={this.state.dataSource}
-        renderItem={({item}) => this.renderNativeItem(item)}
-      />
+      <FlatList data={data} renderItem={({item}) => renderNativeItem(item)} />
     </View>
   );
 
-  _handleIndexChange = (index: any) => this.setState({index});
+  const renderNativeItem = item => {
+    const name = item.name.first + ' ' + item.name.last;
+    return <UserItem fullname={name} email={item.email} />;
+  };
 
-  _renderLazyPlaceholder = ({route}) => <LazyPlaceholder route={route} />;
+  const LazyPlaceholder = ({route}) => (
+    <View style={styles.scene}>
+      <Text>Loading {route.title}…</Text>
+    </View>
+  );
 
-  render() {
-    return (
-      <TabView
-        lazy
-        navigationState={this.state}
-        renderScene={SceneMap({
-          first: this.FirstRoute,
-          second: this.SecondRoute,
-        })}
-        renderLazyPlaceholder={this._renderLazyPlaceholder}
-        onIndexChange={this._handleIndexChange}
-        initialLayout={{width: Dimensions.get('window').width}}
-        style={styles.container}
-      />
-    );
-  }
-}
+  const handleIndexChange = (index: any) => setIndex(index);
+
+  const renderLazyPlaceholder = ({route}) => <LazyPlaceholder route={route} />;
+
+  return (
+    <TabView
+      lazy
+      navigationState={{index, routes}}
+      renderScene={SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+      })}
+      renderLazyPlaceholder={renderLazyPlaceholder}
+      onIndexChange={handleIndexChange}
+      initialLayout={{width: Dimensions.get('window').width}}
+      style={styles.container}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -109,9 +102,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
+  // item: {
+  //   padding: 20,
+  //   marginVertical: 8,
+  //   marginHorizontal: 16,
+  // },
 });
+
+export default App;
